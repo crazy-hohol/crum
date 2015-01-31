c0d01d454ca6a1c0d0d984aa15734ca255c036d9
Artem <martynenko89@gmail.com>
2015-01-31 20:47:48 +0200

    ticket controller js

--- public/js/app/views/TicketView.js
+++ public/js/app/views/TicketView.js
@@ -4,6 +4,7 @@ app.TicketView = Backbone.View.extend({
     tagName: "div",
     template: $("#ticketTemplate").html(),
     render: function () {
+        console.info(this.model);
         var tmpl = _.template(this.template);
         this.$el.html(tmpl(this.model.toJSON()));
     }
