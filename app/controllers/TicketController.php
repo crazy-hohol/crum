32baeccd8ce0cf50564fa146a5e514f25b943b59
Artem <martynenko89@gmail.com>
2015-03-07 17:14:20 +0200

    ticket controller js

--- app/controllers/TicketController.php
+++ app/controllers/TicketController.php
@@ -32,7 +32,7 @@ class TicketController extends BaseController
     {
         $data = Input::all();
         $ticket = Ticket::find($data['id']);
-        //die(var_dump($ticket->toArray()));
+        die(var_dump($data));
         $ticket->update(array_intersect($ticket->toArray(), $data));
 
     }
