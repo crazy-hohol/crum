c33ef20e4ff70768ffccb010e793fd93bd923d88
Artem <martynenko89@gmail.com>
2015-04-04 18:36:35 +0300

    router, handlebars, ticket view

--- public/js/app/views/TicketsListView.js
+++ public/js/app/views/TicketsListView.js
@@ -7,7 +7,7 @@ app.TicketsListView = Backbone.View.extend({
     initialize: function () {
         this.$el.html(this.template());
         var collection = this.collection;
-        this.collection.fetch({reset: true});
+        //this.collection.fetch({reset: true});
         this.collection.on("add", function(ticket) {
             var ticketView = new app.TicketView({model: ticket});
             $("#status" + ticket.get('status'), this.$el).append(ticketView.render().el);
