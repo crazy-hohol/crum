1aa4078ff2dde48c070945d63651d7f245f00f79
Artem <martynenko89@gmail.com>
2015-05-10 22:40:04 +0300

    checkList

--- public/js/app/views/CheckListView.js
+++ public/js/app/views/CheckListView.js
@@ -1,8 +1,8 @@
 define(
-    ['backbone', 'jquery', 'handlebars'],
-    function(Backbone, $, Handlebars) {
+    ['backbone', 'jquery', 'handlebars', 'text!templates/CheckListView.handlebars'],
+    function(Backbone, $, Handlebars, template) {
         var CheckListView = Backbone.View.extend({
-            template: Handlebars.compile(),
+            template: Handlebars.compile(template),
             events: {
                 "click .js-edit-check-list-item": 'editCheckListItem',
                 "click .js-add-check-list-item": 'addCheckListItem',
