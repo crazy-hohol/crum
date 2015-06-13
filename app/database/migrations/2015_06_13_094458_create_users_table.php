3f68024892b22a7e79365b9e7449ec4d6694261e
Artem <martynenko89@gmail.com>
2015-06-13 13:57:19 +0300

    users projects

--- app/database/migrations/2015_06_13_094458_create_users_table.php
+++ app/database/migrations/2015_06_13_094458_create_users_table.php
@@ -12,7 +12,37 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		//
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('login', 50)->unique();
+            $table->string('password', 200);
+            $table->string('name', 200);
+            $table->integer('team_id')->unsigned();
+            $table->foreign('team_id')->references('id')->on('teams');
+        });
+
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 200);
+            $table->string('description', 1000);
+            $table->string('who_add', 50);
+        });
+
+        Sheme::create('users_projects', function (Blueprint $table) {
+            $table->increment('id');
+            $table->integer('user_id')->insigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->integer('project_id')->insigned();
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
+
+        Schema::table('tickets', function(Blueprint $table)
+        {
+            $table->integer('project_id')->unsigned();
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
 	}
 
 	/**
